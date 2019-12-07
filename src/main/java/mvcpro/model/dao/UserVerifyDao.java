bc6e5babeda395956f1ff76e46b6070bcd29b250
zfengye <1596863112@qq.com>
2019-12-08 00:53:22 +0800

    3.1

---
+++ src/main/java/mvcpro/model/dao/UserVerifyDao.java
@@ -0,0 +1,11 @@
+package mvcpro.model.dao;
+
+import com.lqing.orm.internal.SimpleDaoImpl;
+import mvcpro.model.entity.UserVerify;
+
+public class UserVerifyDao extends SimpleDaoImpl<UserVerify> {
+    public static void main(String[] args) throws Exception {
+        UserVerifyDao testVerify=new UserVerifyDao();
+        testVerify.list().forEach(System.out::println);
+    }
+}
