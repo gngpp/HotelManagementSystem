74a08b388a5e6e0d50a604f1aa8398030db3a9b3
black-mac <1596863112@qq.com>
2019-12-07 21:57:31 +0800

    3.1

---
+++ src/main/resources/sql表-临时/roomStandard_20191207.sql
@@ -0,0 +1,1 @@
+INSERT INTO `roomStandard` VALUES (1, '总统套房内设', '100', 1000, '这是顶级的总统套房', 100);
---
+++ src/main/resources/sql表-临时/users_20191207.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` VALUES ('feng', '用户', 1, '20181004095', '', '', '');
+INSERT INTO `users` VALUES ('feng', '管理员', 2, 'admin', '', '', '');
