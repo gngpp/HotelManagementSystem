2bc9618658a94d428dc77970a8ffbb736eb8779a
black-mac <1596863112@qq.com>
2019-12-09 16:37:45 +0800

    3.5

--- src/main/java/mvcpro/model/entity/InfoRoom.java
+++ src/main/java/mvcpro/model/entity/InfoRoom.java
@@ -23,9 +23,107 @@ public class InfoRoom extends DefaultEntity {
     private String type;
 
     @AnnotationField(col_name = "area", sql_type = "int",
-                    size = 20, field_name = "area", java_type = "int",
-                    is_autoincrement = false, is_primary_key = false)
+            size = 20, field_name = "area", java_type = "int",
+            is_autoincrement = false, is_primary_key = false)
     private int area;
+
+    @AnnotationField(col_name = "iphone", sql_type = "varchar",
+            size = 11, field_name = "iphone", java_type = "String",
+            is_autoincrement = false, is_primary_key = false)
+    private String iphone;
+
+    public String getIphone() {
+        return iphone;
+    }
+
+    @AnnotationField(col_name = "ps", sql_type = "varchar",
+            size = 11, field_name = "ps", java_type = "String",
+            is_autoincrement = false, is_primary_key = false)
+    private String ps;
+
+    @AnnotationField(col_name = "tv", sql_type = "varchar",
+            size = 10, field_name = "tv", java_type = "String",
+            is_autoincrement = false, is_primary_key = false)
+    private String tv;
+
+    @AnnotationField(col_name = "max_people", sql_type = "int",
+            size = 11, field_name = "max_people", java_type = "Integer",
+            is_autoincrement = false, is_primary_key = false)
+    private Integer max_people;
+
+    @AnnotationField(col_name = "max_bed", sql_type = "int",
+            size = 11, field_name = "max_bed", java_type = "Integer",
+            is_autoincrement = false, is_primary_key = false)
+    private Integer max_bed;
+
+     @AnnotationField(col_name = "rest", sql_type = "varchar",
+            size = 11, field_name = "rest", java_type = "String",
+            is_autoincrement = false, is_primary_key = false)
+    private String rest;
+
+    @AnnotationField(col_name = "air_conditioning", sql_type = "varchar",
+            size = 11, field_name = "air_conditioning", java_type = "String",
+            is_autoincrement = false, is_primary_key = false)
+     private String air_conditioning;
+
+    public String getAir_conditioning() {
+        return air_conditioning;
+    }
+
+    public InfoRoom setAir_conditioning(String air_conditioning) {
+        this.air_conditioning = air_conditioning;
+        return this;
+    }
+
+    public String getRest() {
+        return rest;
+    }
+
+    public InfoRoom setRest(String rest) {
+        this.rest = rest;
+        return this;
+    }
+
+    public Integer getMax_bed() {
+        return max_bed;
+    }
+
+    public InfoRoom setMax_bed(Integer max_bed) {
+        this.max_bed = max_bed;
+        return this;
+    }
+
+    public Integer getMax_people() {
+        return max_people;
+    }
+
+    public InfoRoom setMax_people(Integer max_peple) {
+        this.max_people = max_peple;
+        return this;
+    }
+
+    public String getTv() {
+        return tv;
+    }
+
+    public InfoRoom setTv(String tv) {
+        this.tv = tv;
+        return this;
+    }
+
+    public String getPs() {
+        return ps;
+    }
+
+    public InfoRoom setPs(String ps) {
+        this.ps = ps;
+        return this;
+    }
+
+    public InfoRoom setIphone(String iphone) {
+        this.iphone = iphone;
+        return this;
+    }
 
     public String getType() {
         return type;
@@ -61,5 +159,20 @@ public class InfoRoom extends DefaultEntity {
         this.id = id;
     }
 
-
+    @Override
+    public String toString() {
+        return "InfoRoom{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", area=" + area +
+                ", iphone='" + iphone + '\'' +
+                ", ps='" + ps + '\'' +
+                ", tv='" + tv + '\'' +
+                ", max_people=" + max_people +
+                ", max_bed=" + max_bed +
+                ", rest='" + rest + '\'' +
+                ", air_conditioning='" + air_conditioning + '\'' +
+                '}';
+    }
 }
