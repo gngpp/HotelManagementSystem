e08f4b779188c1ba7da7528c639f2eda84542e20
Windows <1596863112@qq.com>
2019-12-09 23:45:05 +0800

    3.7

--- src/main/java/mvcpro/model/dao/InfoRoomDao.java
+++ src/main/java/mvcpro/model/dao/InfoRoomDao.java
@@ -2,10 +2,7 @@ package mvcpro.model.dao;
 
 
 import com.lqing.orm.internal.SimpleDaoImpl;
-import com.lqing.orm.test.Student;
 import mvcpro.model.entity.InfoRoom;
-import mvcpro.model.entity.Test;
-import mvcpro.model.entity.User;
 
 public class InfoRoomDao extends SimpleDaoImpl<InfoRoom> {
     public static void main(String[] args) throws Exception {
