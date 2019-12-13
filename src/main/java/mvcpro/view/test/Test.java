83f7f1e2c30f601ee9a6a5d00596e6030342889b
zfengye <1596863112@qq.com>
2019-12-13 13:51:35 +0800

    4.0

--- src/main/java/mvcpro/view/test/Test.java
+++ src/main/java/mvcpro/view/test/Test.java
@@ -1,13 +1,21 @@
 package mvcpro.view.test;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Test {
-    public static void main(String[] args) {
-        String filePath=Test.class.getResource("./")+ new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date())+".sql";
-        System.out.println(filePath);
-        System.out.println(new File(filePath).getAbsolutePath());
+    public static void main(String[] args) throws IOException {
+        File file=new File(new File("").getCanonicalPath()+"/src/main/resources/backup/schema/");
+        File newFile=new File(file,new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())+"my.sql");
+        if (!file.exists()){
+            file.mkdir();
+        }
+        if (!newFile.exists()){
+            newFile.createNewFile();
+        }
+
+        System.out.println(newFile);
     }
 }
