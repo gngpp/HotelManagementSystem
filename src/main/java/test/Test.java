53107ee46576abe9b8c8fc744a3bb43ec8452e1b
zfengye <1596863112@qq.com>
2019-12-14 22:40:26 +0800

    4.2

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -1,5 +1,8 @@
 package test;
 
+import com.lqing.orm.internal.connection.C3p0ConnectionProvider;
+import com.lqing.orm.internal.connection.IConnectionProvide;
+import com.sun.javaws.IconUtil;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -18,24 +21,26 @@ import java.awt.*;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.Statement;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Test {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        File file = new File(new File("").getCanonicalPath() + "/src/main/resources/backup/schema/");
-        File newFile = new File(file, new SimpleDateFormat("YYYY-MM-dd-hh:mm:ss").format(new Date()) + "-backup");
-        if (!file.exists()) {
-            file.mkdir();
-        }
-        System.out.println(System.getProperties().get("os.name"));
-        System.out.println(newFile);
+    private  IConnectionProvide connProvider = new C3p0ConnectionProvider();
+
+    public static void main(String[] args) throws Exception {
 
 
-        //备份
-        String mysql="/usr/local/mysql/bin/mysql -h localhost --user=root --password=itcast --result-file=/Users/black-mac/IdeaProjects/HotelManagementSystem-fx/src/main/resources/backup/schema/2019-12-14-09:03:54-backup--default-character-set=utf8.sql FXdb";
-        System.out.println(mysql);
+//        File file = new File(new File("").getCanonicalPath() + "/src/main/resources/backup/schema/");
+//        File newFile = new File(file, new SimpleDateFormat("YYYY-MM-dd-hh:mm:ss").format(new Date()) + "-backup");
+//        if (!file.exists()) {
+//            file.mkdir();
+//        }
+//        System.out.println(System.getProperties().get("os.name"));
+//
+//        //备份
 //        String mysqldump = "/usr/local/mysql/bin/mysqldump --opt -h localhost --user=root --password=itcast --result-file=" + newFile.toString() + "--default-character-set=utf8.sql FXdb";
 //        System.out.println(mysqldump);
 //        Process p = Runtime.getRuntime().exec(mysqldump);
@@ -44,57 +49,103 @@ public class Test {
 //        } else {
 //            System.out.println("导出失败,失败码:" + p.waitFor());
 //        }
+        Test test=new Test();
+      //  System.out.println(test.readFileByLines("/Volumes/Backup/javaFX期末项目用件/HotelManagementSystem-fx/src/main/resources/backup/schema/2019-12-14-10:23:17-backup--default-character-set=utf8.sql"));
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        test.runSqlByReadFileContent("/Volumes/Backup/javaFX期末项目用件/HotelManagementSystem-fx/src/main/resources/backup/schema/2019-12-14-10:34:11-backup--default-character-set=utf8.sql");
+    }
+
+
+
+    /**
+     * @方法描述：批量执行SQL语句
+     * url加上，多条sql语句执行，allowMultiQueries=true
+     * @param sql 包含待执行的SQL语句的ArrayList集合
+     * @return int 影响的函数
+     */
+    public int batchDate(ArrayList<String> sql){
+        try {
+            Statement st = connProvider.getConnection().createStatement();
+            for(String subsql :sql){
+                st.addBatch(subsql);
+            }
+            st.executeBatch();
+            return 1;
+        }catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+
+    private ArrayList<String> readFileByLines(String filePath) throws Exception {
+        ArrayList<String> listStr=new ArrayList<>();
+        StringBuffer sb=new StringBuffer();
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new InputStreamReader(
+                    new FileInputStream(filePath), "UTF-8"));
+            String tempString = null;
+            int flag=0;
+            // 一次读入一行，直到读入null为文件结束
+            while ((tempString = reader.readLine()) != null) {
+                // 显示行号
+                // System.out.println("line " + line + ": " + tempString);
+                if(tempString.trim().equals(""))
+                    continue;
+                if(tempString.substring(tempString.length()-1).equals(";")){
+                    if(flag==1){
+                        sb.append(tempString);
+                        listStr.add(sb.toString());
+                        sb.delete(0,sb.length());
+                        flag=0;
+                    }
+                    else
+                        listStr.add(tempString);
+                }else{
+                    flag=1;
+                    sb.append(tempString);
                 }
             }
-        }).start();
-        Process ps = Runtime.getRuntime().exec(mysql);
-        if (ps.waitFor() == 0) {// 0 表示线程正常终止。
-            System.out.println("恢复成功");
-        } else {
-            System.out.println("恢复失败,失败码:" + ps.waitFor());
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                }
+            }
         }
 
-
-
-
+        return listStr;
     }
 
-}
-
-class Demo extends Application {
-    public static void main(String[] args) throws IOException {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Button button=new Button("button");
-        button.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                FileChooser fileChooser=new FileChooser();
-                File file=fileChooser.showOpenDialog(new Stage());
-                try {
-                    System.out.println(file.getCanonicalPath());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+    /**
+     * 读取文件内容到SQL中执行
+     * @param sqlPath SQL文件的路径：如：D:/TestProject/web/sql/脚本.Sql
+     */
+    public void runSqlByReadFileContent(String sqlPath) {
+        try {
+            ArrayList<String> sqlStr = readFileByLines(sqlPath);
+            System.out.println(sqlStr);
+            if (sqlStr.size() > 0) {
+                int num = batchDate(sqlStr);
+                if (num > 0)
+                    System.out.println("执行成功");
+                else
+                    System.out.println("未有执行的SQL语句");
+            } else {
+                System.out.println("没有需要执行的SQL语句");
             }
-        });
-        Pane pane=new AnchorPane();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        pane.getChildren().add(button);
-        primaryStage.setScene(new Scene(pane,300,300));
-        primaryStage.show();
     }
 }
+
 
 
---
+++ src/main/resources/backup/schema/2019-12-14-10:34:11-backup--default-character-set=utf8.sql
@@ -0,0 +1,230 @@
+-- MySQL dump 10.13  Distrib 5.7.10, for osx10.9 (x86_64)
+--
+-- Host: localhost    Database: FXdb
+-- ------------------------------------------------------
+-- Server version	5.7.10
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `booking_room`
+--
+
+DROP TABLE IF EXISTS `booking_room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `booking_room` (
+  `room_id` int(20) NOT NULL AUTO_INCREMENT COMMENT '客房编号/不显示',
+  `room_type` char(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '客房类型',
+  `room_price` int(11) DEFAULT NULL COMMENT '客房单价',
+  `room_peple_id` char(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '客户编号',
+  `room_in_date` datetime DEFAULT NULL COMMENT '入住日期',
+  `room_discount` char(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '折扣',
+  `room_out_date` datetime DEFAULT NULL COMMENT '结算日起',
+  `room_amout` int(20) DEFAULT NULL COMMENT '总额',
+  `room_peple_name` char(10) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '客户姓名',
+  `room_id_number` int(11) DEFAULT NULL,
+  `room_remark` char(200) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`room_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking_room`
+--
+
+LOCK TABLES `booking_room` WRITE;
+/*!40000 ALTER TABLE `booking_room` DISABLE KEYS */;
+INSERT INTO `booking_room` VALUES (1,'总统套房',1000,'100','2019-07-07 00:00:00','未知','2019-08-08 00:00:00',1000,'张三',110,'这是总统套房');
+/*!40000 ALTER TABLE `booking_room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `client`
+--
+
+DROP TABLE IF EXISTS `client`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `client` (
+  `client_name` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `client_sex` char(2) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `client_native` varchar(30) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `client_id` int(20) NOT NULL AUTO_INCREMENT,
+  `client_phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `client_id_card` char(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `client_id_number` char(20) COLLATE utf8_unicode_ci DEFAULT NULL COMMENT '作为逻辑主键',
+  PRIMARY KEY (`client_id`),
+  KEY `client_id_number` (`client_id_number`)
+) ENGINE=InnoDB AUTO_INCREMENT=2202032 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `client`
+--
+
+LOCK TABLES `client` WRITE;
+/*!40000 ALTER TABLE `client` DISABLE KEYS */;
+/*!40000 ALTER TABLE `client` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `info_room`
+--
+
+DROP TABLE IF EXISTS `info_room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `info_room` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` char(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `area` int(11) DEFAULT NULL,
+  `iphone` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `ps` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `tv` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `max_people` int(11) DEFAULT NULL,
+  `max_bed` int(11) DEFAULT NULL,
+  `rest` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `air_conditioning` varchar(11) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `fk_room_id` FOREIGN KEY (`id`) REFERENCES `standard_room` (`room_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `info_room`
+--
+
+LOCK TABLES `info_room` WRITE;
+/*!40000 ALTER TABLE `info_room` DISABLE KEYS */;
+/*!40000 ALTER TABLE `info_room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `standard_room`
+--
+
+DROP TABLE IF EXISTS `standard_room`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `standard_room` (
+  `room_id` int(20) NOT NULL AUTO_INCREMENT,
+  `room_type` char(20) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `room_floor` char(10) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `room_price` int(20) DEFAULT NULL,
+  `room_remark` char(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `room_id_number` int(20) DEFAULT NULL,
+  PRIMARY KEY (`room_id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=44030 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `standard_room`
+--
+
+LOCK TABLES `standard_room` WRITE;
+/*!40000 ALTER TABLE `standard_room` DISABLE KEYS */;
+INSERT INTO `standard_room` VALUES (44002,'豪华间/高级间','二楼',2323,'....',101),(44007,'单身人间','二楼',222,'2222',102),(44020,'单身人间','二楼',222,'2222',103),(44027,'标准间','一楼',222,'2231',104),(44028,'标准间','一楼',9999,'0-0-',105),(44029,'标准间','一楼',90,'最后一个憨逼。。。',106);
+/*!40000 ALTER TABLE `standard_room` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` char(24) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=110 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `students`
+--
+
+LOCK TABLES `students` WRITE;
+/*!40000 ALTER TABLE `students` DISABLE KEYS */;
+INSERT INTO `students` VALUES (5,'n'),(10,'hanbi'),(11,'hanbi'),(12,'hanbi'),(13,'hanbi'),(14,'hanbi'),(15,'hanbi'),(16,'hanbi'),(17,'hanbi'),(18,'hanbi'),(19,'hanbi'),(20,'hanbi'),(21,'hanbi'),(22,'hanbi'),(23,'hanbi'),(24,'hanbi'),(25,'hanbi'),(26,'hanbi'),(27,'hanbi'),(28,'hanbi'),(29,'hanbi'),(30,'hanbi'),(31,'hanbi'),(32,'hanbi'),(33,'hanbi'),(34,'hanbi'),(35,'hanbi'),(36,'hanbi'),(37,'hanbi'),(38,'hanbi'),(39,'hanbi'),(40,'hanbi'),(41,'hanbi'),(42,'hanbi'),(43,'hanbi'),(44,'hanbi'),(45,'hanbi'),(46,'hanbi'),(47,'hanbi'),(48,'hanbi'),(49,'hanbi'),(50,'hanbi'),(51,'hanbi'),(52,'hanbi'),(53,'hanbi'),(54,'hanbi'),(55,'hanbi'),(56,'hanbi'),(57,'hanbi'),(58,'hanbi'),(59,'hanbi'),(60,'hanbi'),(61,'hanbi'),(62,'hanbi'),(63,'hanbi'),(64,'hanbi'),(65,'hanbi'),(66,'hanbi'),(67,'hanbi'),(68,'hanbi'),(69,'hanbi'),(70,'hanbi'),(71,'hanbi'),(72,'hanbi'),(73,'hanbi'),(74,'hanbi'),(75,'hanbi'),(76,'hanbi'),(77,'hanbi'),(78,'hanbi'),(79,'hanbi'),(80,'你就是个憨逼'),(81,'你就是个憨逼'),(100,'988'),(101,'你就是个憨逼'),(102,'你就是个憨逼'),(103,'200'),(104,'200'),(105,'200'),(106,'200'),(107,'200'),(108,'200'),(109,'200');
+/*!40000 ALTER TABLE `students` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `password` char(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '123456',
+  `userType` char(3) COLLATE utf8_unicode_ci NOT NULL DEFAULT '用户',
+  `UUID` int(11) NOT NULL AUTO_INCREMENT,
+  `id` char(15) COLLATE utf8_unicode_ci NOT NULL,
+  `question_two` char(255) COLLATE utf8_unicode_ci NOT NULL,
+  `question_three` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `question_one` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `picture` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`UUID`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES ('e10adc3949ba59abbe56e057f20f883e','用户',1,'20181004095','1','1','1','/png/屏幕快照 2019-11-23 18.40.07.png'),('e10adc3949ba59abbe56e057f20f883e','管理员',2,'admin','1','1','1','/png/128x128-ic_perm_identity_48px.png'),('e10adc3949ba59abbe56e057f20f883e','管理员',31,'1596863112','n','n','nn','/png/屏幕快照 2019-11-23 18.40.07.png');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users_verify`
+--
+
+DROP TABLE IF EXISTS `users_verify`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users_verify` (
+  `UUID` int(11) NOT NULL AUTO_INCREMENT,
+  `select_one` char(255) COLLATE utf8_unicode_ci NOT NULL,
+  `select_two` char(255) COLLATE utf8_unicode_ci NOT NULL,
+  `select_three` char(255) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`UUID`),
+  CONSTRAINT `fk_verify_referenced_users` FOREIGN KEY (`UUID`) REFERENCES `users` (`UUID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users_verify`
+--
+
+LOCK TABLES `users_verify` WRITE;
+/*!40000 ALTER TABLE `users_verify` DISABLE KEYS */;
+INSERT INTO `users_verify` VALUES (1,'你的生日是?','您父亲的姓名是？','您的出生地是？'),(2,'你的生日是?','您父亲的姓名是？','您的出生地是？'),(31,'你的生日是?','您父亲的姓名是？','您的出生地是？');
+/*!40000 ALTER TABLE `users_verify` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2019-12-14 22:34:11
