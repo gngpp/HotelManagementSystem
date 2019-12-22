6130aa271420e21942354597cc993609ca8eba3e
Windows <1596863112@qq.com>
2019-12-22 12:03:10 +0800

    5.8

--- src/main/java/com/lqing/orm/utils/ConstantStringPool.java
+++
@@ -1,7 +0,0 @@
-package com.lqing.orm.utils;
-
-public class ConstantStringPool {
-    public static final String GET_METHOD_KEY_PREFIX = "get";
-    public static final String SET_METHOD_KEY_PREFIX = "set";
-    public static final String GET_BOOLEAN_METHOD_KEY_PREFIX = "is";
-}
--- src/main/java/mvcpro/controlled/VerifyCard.java
+++
@@ -1,5 +0,0 @@
-package mvcpro.controlled;
-
-public abstract class VerifyCard {
-    public final String USER="用户";
-}
